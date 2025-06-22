ab9df840fae132e32e42962d203e670b4e0fd00d
Walentalien <voliinyk@student.agh.edu.pl>
2025-06-22 11:06:52 +0200

    Added new example

---
+++ Automates/ex_from_class.js
@@ -0,0 +1,22 @@
+// Generated automatically by nearley, version 2.20.1
+// http://github.com/Hardmath123/nearley
+(function () {
+function id(x) { return x[0]; }
+var grammar = {
+    Lexer: undefined,
+    ParserRules: [
+    {"name": "S", "symbols": ["E"]},
+    {"name": "E", "symbols": ["T"]},
+    {"name": "E", "symbols": ["E", {"literal":"+"}, "T"]},
+    {"name": "T", "symbols": ["P"]},
+    {"name": "T", "symbols": ["T", {"literal":"*"}, "P"]},
+    {"name": "P", "symbols": [{"literal":"a"}]}
+]
+  , ParserStart: "S"
+}
+if (typeof module !== 'undefined'&& typeof module.exports !== 'undefined') {
+   module.exports = grammar;
+} else {
+   window.grammar = grammar;
+}
+})();
